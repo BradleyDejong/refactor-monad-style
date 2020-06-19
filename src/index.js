a0f2d3bf8b3853501168ece6475b6f47feaf7bb4
Bradley DeJong <DeJong.Bradley@mayo.edu>
2020-06-19 12:57:49 -0500

    Add View type

--- src/index.js
+++ src/index.js
@@ -1,56 +1,83 @@
 import html from "nanohtml";
-import morphdom from "morphdom";
+import nanomorph from "nanomorph";
+
+const View = (render) => ({
+  render: render,
+  contramap: (adapterFn) =>
+    View((state, dispatch) => render(adapterFn(state), dispatch)),
+  concat: (otherView) =>
+    View(
+      (state, dispatch) => html`
+        ${render(state, dispatch)} ${otherView.render(state, dispatch)}
+      `
+    ),
+});
 
 const header = () => html`<h1>World's best app</h1>`;
 
-const renderRefresh = (state, dispatch) => html`
+const renderRefresh = (lastUpdated, dispatch) => html`
   <div>
-    Last updated at ${state.lastUpdated.toLocaleString()}.
+    Last updated at ${lastUpdated.toLocaleString()}.
   </div>
   <button onclick=${() => dispatch("update")}>
     Click To Update
   </button>
 `;
 
-const renderClicky = (state, dispatch) => html`
-  <div>
-    You've clicked ${state.clicks} times
-  </div>
-  <button onclick=${() => dispatch("clicked")}>
-    Click Me
-  </button>
-`;
+const renderClicky = View(
+  (clicks, dispatch) => html`
+    <div>
+      You've clicked ${clicks} times
+    </div>
+    <button onclick=${() => dispatch("clicked")}>
+      Click Me
+    </button>
+  `
+);
 
-const renderDecorations = (state, dispatch) => html`
-  <div class="decoration">
-    insert decoration here
-  </div>
-  <span class="decoration"> 🦄🦄🦄🦄🦄🦄 </span>
-`;
+const renderDecorations = View(
+  (state, dispatch) => html`
+    <div class="decoration">
+      insert decoration here
+    </div>
+    <span class="decoration"> 🦄🦄🦄🦄🦄🦄 </span>
+  `
+);
+
+const refreshAndClicky = View(renderRefresh)
+  .contramap((s) => s.lastUpdated)
+  .concat(renderClicky.contramap((s) => s.clicks))
+  .concat(renderDecorations.contramap((s) => undefined));
 
 const content = (state, dispatch) => html`
-  <div class="content">
-    ${renderRefresh(state, dispatch)} ${renderDecorations()}
-    ${renderClicky(state, dispatch)}
+  <div id="content" class="content">
+    ${refreshAndClicky.render(state, dispatch)}
+    <div>Total clicks:</div>
+    <div>${state.totalClicks}</div>
   </div>
 `;
 
-const render = ({ state, dispatch }) => html`<div id="app">
-  ${header(state, dispatch)} ${content(state, dispatch)}
-</div>`;
+const wholeApp = View(
+  ({ state, dispatch }) => html`<div id="app">
+    ${header(state, dispatch)} ${content(state, dispatch)}
+  </div>`
+);
 
 const reduce = (state, event) => {
   if (event === "clicked") {
     return {
       ...state,
       clicks: state.clicks + 1,
+      totalClicks: state.totalClicks + 1,
     };
   } else if (event === "update") {
     return {
       ...state,
       lastUpdated: new Date(),
+      totalClicks: state.totalClicks + 1,
     };
   }
+
   return state;
 };
 
@@ -59,10 +86,11 @@ const app = document.getElementById("app");
 const state = {
   lastUpdated: new Date(),
   clicks: 0,
+  totalClicks: 0,
 };
 
 const rerender = (state, dispatch) =>
-  morphdom(app, render({ state, dispatch }));
+  nanomorph(app, wholeApp.render({ state, dispatch }));
 
 const dispatch = (event) => {
   const newState = reduce(state, event);
