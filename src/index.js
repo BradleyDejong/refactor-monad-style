385729d89e91ef20a030856d630095688fce4b69
Bradley DeJong <DeJong.Bradley@mayo.edu>
2020-08-14 13:01:30 -0500

    Add beginning side effects

--- src/index.js
+++ src/index.js
@@ -43,11 +43,22 @@ const makeGreenText = (v) => {
 const readerWithAdapter = (adapterFn) => (r) =>
   r.map((v) => v.contramap(adapterFn));
 
+const quoteView = ask(prop("dispatch")).map((d) =>
+  View(
+    (q) =>
+      html`<div>Quote:</div>
+        <div>${q}</div>
+        <div></div>
+        <button onclick=${() => d("QUOTE")}>Get new quote</button>`
+  )
+);
+
 const children = [
   renderRefresh.map((v) => v.contramap((s) => s.lastUpdated)),
   renderClickMe.map((v) => v.contramap((s) => s.clicks)),
   Reader.of(renderDecorations.contramap((s) => undefined)),
   Reader.of(totalClicks.contramap((s) => s.totalClicks)),
+  quoteView.map((v) => v.contramap((s) => s.quote)),
 ];
 
 const contentViews = children.reduce(concat, Reader.of(View.empty));
@@ -63,12 +74,6 @@ const headerReader = ask(prop("title")).map((title) =>
 );
 
 const appHeader = headerReader.map((r) => r.map(headerHtml));
-
-const dispatchGlobal = (event) => {
-  const newState = reduce(state, event);
-  Object.assign(state, newState);
-  rerender(state, dispatchGlobal);
-};
 
 const wholeApp = Reader.of(View.of(contentHeaderHtml))
   .concat(content)
@@ -113,16 +118,27 @@ const reducer2 = Reader((action) =>
   )
 );
 
+const quoteReducer = Reader((action) =>
+  Endo((state) =>
+    action.type && action.type === "QUOTE_UPDATED"
+      ? { ...state, quote: action.quote }
+      : state
+  )
+);
+
 const actionCounter = (state) => ({
   ...state,
   totalClicks: state.totalClicks + 1,
 });
 
-const allOurReducers = [reducer1, reducer2, Reader.of(Endo(actionCounter))];
+const allOurReducers = [
+  reducer1,
+  reducer2,
+  Reader.of(Endo(actionCounter)),
+  quoteReducer,
+];
 
 const reduce = (state, event) => {
-  console.log("EVENT", event);
-
   return allOurReducers
     .reduce(concat, Reader.of(Endo.empty))
     .run(event) // Runs the reader
@@ -135,6 +151,7 @@ const state = {
   lastUpdated: new Date(),
   clicks: 0,
   totalClicks: 0,
+  quote: "Ken is the best. -Ryan",
 };
 
 const rerender = (state, dispatch) =>
@@ -148,5 +165,38 @@ const rerender = (state, dispatch) =>
       })
       .render(state)
   );
+
+const updateState = (event) => {
+  const newState = reduce(state, event);
+  Object.assign(state, newState);
+};
+
+// action1 :: Action -> Promise Action
+const action1 = (e) =>
+  e === "QUOTE"
+    ? [
+        fetch("https://api.quotable.io/random")
+          .then((res) => res.json())
+          .then((body) => {
+            const quote = body.content;
+            console.log("GOT QUOTE", quote);
+
+            return { type: "QUOTE_UPDATED", quote };
+          }),
+      ]
+    : [];
+
+const runEffects = (event) => {
+  return action1(event);
+};
+
+const dispatchGlobal = (event) => {
+  console.log("EVENT", event);
+  updateState(event);
+  const nextActions = runEffects(event, dispatchGlobal);
+
+  nextActions.forEach((p) => p.then(dispatchGlobal));
+  rerender(state, dispatchGlobal);
+};
 
 rerender(state, dispatchGlobal); // start the app
